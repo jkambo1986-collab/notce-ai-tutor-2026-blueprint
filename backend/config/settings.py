5f04896a0444194a4161ad0c2e68ad0805c7bb05
jkambo1986-collab <jkambo1986@gmail.com>
2026-01-20 12:04:46 -0700

    Allow all hosts to avoid 400 errors during setup

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ks^u!^98ntxpiqp06)l5x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
